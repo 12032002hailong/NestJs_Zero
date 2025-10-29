f9c11fe799c822ec8e3a4597b309442e49f40624
Hai Long <hailongkc1203@gmail.com>
2025-10-29 16:11:07 +0700

    Validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
+import { Transform } from 'class-transformer';
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @Transform(({ value }) => value?.trim().toLowerCase())
+  @IsNotEmpty({ message: 'Email không được để trống' })
+  @IsEmail({}, { message: 'Email không đúng định dạng' })
   email: string;
+
+  @IsNotEmpty({
+    message: 'Password không được để trống',
+  })
   password: string;
+
   name: string;
 }
