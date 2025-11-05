b31dc48ca076ef06041a6b91e85e6efc2659c4f7
Hai Long <hailongkc1203@gmail.com>
2025-11-05 17:30:09 +0700

    passing req.user

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -1,14 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Req,
+} from '@nestjs/common';
 import { CompaniesService } from './companies.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
-
+import { User } from 'src/decorator/customize';
+import type { IUser } from 'src/users/user.interface';
 @Controller('companies')
 export class CompaniesController {
   constructor(private readonly companiesService: CompaniesService) {}
 
   @Post()
-  create(@Body() createCompanyDto: CreateCompanyDto) {
+  create(@Body() createCompanyDto: CreateCompanyDto, @User() user: IUser) {
     return this.companiesService.create(createCompanyDto);
   }
 
